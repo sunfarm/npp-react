23e31784054c10d144800f734632f2aece17dbf4
Nathanael Raway <nathanael.raway@gmail.com>
2020-08-18 12:32:36 -0500

    Added initial console stuff

--- src/App.js
+++ src/App.js
@@ -2,6 +2,44 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+          showSnippet: false,
+          output: ''
+        }
+    }
+
+    out = (text) => {
+        console.log(text);
+        this.setState({
+            output: `${this.state.output}\n${text}` ,
+        });
+        window.scrollTo(0,document.body.scrollHeight);
+
+    }
+
+    componentDidMount = () => {
+        console.log("Welcome to the Nerd++ site!");
+
+        window.out = this.out;
+
+        window.hello = () => {
+            this.out("Hi!");
+        }
+
+        window.snip = () => {
+            this.setState({
+                showSnippet: true
+            });
+        }
+
+        window.tip = () => {
+            this.out('Here is a helpful tip');
+        }
+    }
+
     render() {
         const today = new Date();
         const tabs = today.getHours() % 12 + 1;
@@ -13,6 +51,8 @@ class App extends Component {
                         <span className="red">Nerd</span>
                         <span className="blue">++</span>
                         {/* <span className="light-grey">;</span> */}
+                        { this.state.showSnippet ? <pre className="light-grey">This will be a useful code snippet</pre> : ''}
+                        { this.state.output ? <pre className="light-grey">{this.state.output}</pre> : ''}
                     </h1>
                 </header>
             </div>
